8a971c6d487ee0a2b2b858fa9c8b9f65f600a4f6
ptzxazx1387-stack <pt.zx.azx.1387@gmail.com>
2026-07-19 21:01:32 +0330

    Update esp.h

--- esp.h
+++ esp.h
@@ -8,7 +8,7 @@ namespace ESP {
     inline void Render(const std::vector<PlayerData>& players, Matrix viewMatrix, int width, int height, float fps) {
         char watermark[64];
         sprintf_s(watermark, "by .presidental - FPS: %.0f", fps);
-        ImGui::GetOverlayDrawList()->AddText(ImVec2(10, 10), IM_COL32(255, 255, 255, 255), watermark);
+        ImGui::GetForegroundDrawList()->AddText(ImVec2(10, 10), IM_COL32(255, 255, 255, 255), watermark);
 
         for (const auto& player : players) {
             if (!player.isValid || player.position.empty()) continue;
@@ -20,7 +20,7 @@ namespace ESP {
                 std::string label = player.name;
                 if (player.isSleeping) label += " [sleeping]";
 
-                ImGui::GetOverlayDrawList()->AddText(
+                ImGui::GetForegroundDrawList()->AddText(
                     ImVec2(screenPos.x, screenPos.y - 15.0f),
                     col,
                     label.c_str()
